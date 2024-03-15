24d66eb8a515cbc28c83ce02370a81228cfa9460
SHEIKH NASIR KAMARUDIN <sheikhnasir@yahoo.com>
2024-03-15 23:06:48 +0800

    Updated SQL

--- src/java/users/Users.java
+++ src/java/users/Users.java
@@ -5,7 +5,6 @@
  */
 package users;
 
-
 import db.dbcon;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -20,136 +19,130 @@ import java.util.logging.Logger;
  * @author virtualspace
  */
 public class Users {
+
     private String userid;
     private String password;
     private String name;
     private String email;
     private String tel;
     private String userinterface;
-    
-     public Users(){
-         
-     }        
 
-      public Users(String username, String password, String name) {
+    public Users() {
+
+    }
+
+    public Users(String username, String password, String name) {
         this.userid = username;
         this.password = password;
-        this.name=name;
+        this.name = name;
     }
 
-     public int checkUserStatus() {
-          ResultSet rs=null;
-          int x=0;
+    public int checkUserStatus() {
+        ResultSet rs = null;
+        int x = 0;
         try {
-          
-            db.dbcon query1=new db.dbcon();
-            
-            String sql=
-                    "SELECT\n" +
-                        "\"public\".tbluser.\"UserID\",\n" +
-                        "\"public\".tbluser.\"Password\",\n" +
-                        "\"public\".tbluser.\"UserType\",\n" +
-                        "\"public\".category.\"Description\",\n" +
-                        "\"public\".category.\"Interface\"\n" +
-                        "FROM\n" +
-                        "\"public\".category\n" +
-                        "INNER JOIN \"public\".tbluser ON \"public\".tbluser.\"UserType\" = \"public\".category.\"UserType\"\n" +
-                        "WHERE\n" +
-                        "\"public\".tbluser.\"UserID\" = '"+this.getUserid().trim()+"' and\n" +
-                        "\"public\".tbluser.\"Password\"='"+this.getPassword().trim()+"'";
 
-            rs=query1.sqlquery(sql);
+            db.dbcon query1 = new db.dbcon();
+
+            String sql
+                    = "SELECT\n"
+                    + "count(*) as rowcount\n"
+                    + "FROM\n"
+                    + "\"public\".category\n"
+                    + "INNER JOIN \"public\".tbluser ON \"public\".tbluser.\"UserType\" = \"public\".category.\"UserType\"\n"
+                    + "WHERE\n"
+                    + "\"public\".tbluser.\"UserID\" = '" + this.getUserid().trim() + "' and\n"
+                    + "\"public\".tbluser.\"Password\"='" + this.getPassword().trim() + "'";
+
+            rs = query1.sqlquery(sql);
             rs.next();
-            x=rs.getInt("rowcount");
-          
-        } 
-        catch (Exception ex){
+            x = rs.getInt("rowcount");
+
+        } catch (Exception ex) {
             Logger.getLogger(Users.class.getName()).log(Level.SEVERE, null, ex);
         }
-      /*  catch (SQLException ex) {
-            Logger.getLogger(Users.class.getName()).log(Level.SEVERE, null, ex);
-        }*/
+        /*  catch (SQLException ex) {
+         Logger.getLogger(Users.class.getName()).log(Level.SEVERE, null, ex);
+         }*/
         return x;
     }
-      public boolean updateUser(String u, String p,String n,String e){
-       int rowAffected=0;
-       try {
-         // String sql="INSERT INTO tbluser (username,name,dob,password)"
-         //         + "VALUES ('"+u+"','"+n+"',"+ "null"+",'"+p+"')";
-           String sql = "UPDATE tbluser SET name = '"+n+"',"
-                   + " dob = '"+ e +"', password = '"+p+"' WHERE username = '"+u+"'";
 
-            dbcon query1=new dbcon();
-             Class.forName("org.apache.derby.jdbc.ClientDriver");
-            //Get a connection
-         
-            connection = DriverManager.getConnection(DB_URL); 
-           statement=connection.createStatement();
-             rowAffected= statement.executeUpdate(sql);
-        
-      }
-       catch (Exception ex) {
-          Logger.getLogger(Users.class.getName()).log(Level.SEVERE, null, ex);
-      }
-     if ( rowAffected==1){
-         return true;
-     }else
-     {
-          return false;
-     }
-  }
-    void updatename() {
-          ResultSet rs=null;
-        
+    public boolean updateUser(String u, String p, String n, String e) {
+        int rowAffected = 0;
         try {
-          
-            dbcon query1=new dbcon();
-            String sql="";
-            sql="select name from tbluser where username='"+this.getName()+"'";
-            rs=query1.sqlquery(sql);
+         // String sql="INSERT INTO tbluser (username,name,dob,password)"
+            //         + "VALUES ('"+u+"','"+n+"',"+ "null"+",'"+p+"')";
+            String sql = "UPDATE tbluser SET name = '" + n + "',"
+                    + " dob = '" + e + "', password = '" + p + "' WHERE username = '" + u + "'";
+
+            dbcon query1 = new dbcon();
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            //Get a connection
+
+            connection = DriverManager.getConnection(DB_URL);
+            statement = connection.createStatement();
+            rowAffected = statement.executeUpdate(sql);
+
+        } catch (Exception ex) {
+            Logger.getLogger(Users.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        if (rowAffected == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    void updatename() {
+        ResultSet rs = null;
+
+        try {
+
+            dbcon query1 = new dbcon();
+            String sql = "";
+            sql = "select name from tbluser where username='" + this.getName() + "'";
+            rs = query1.sqlquery(sql);
             rs.next();
             //this.name=rs.getString("name");
-            
+
           //  String n = rs.getString(3);
-          
         } catch (SQLException ex) {
             Logger.getLogger(Users.class.getName()).log(Level.SEVERE, null, ex);
         }
-       
+
     }
-    static final String DB_URL =
-      "jdbc:derby://localhost:1527/sample;;create=true;user=app;password=app";
-   static final String DB_DRV =
-      "org.apache.derby.jdbc.ClientDriver";
+    static final String DB_URL
+            = "jdbc:derby://localhost:1527/sample;;create=true;user=app;password=app";
+    static final String DB_DRV
+            = "org.apache.derby.jdbc.ClientDriver";
    //static final String DB_USER = "root";
-   //static final String DB_PASSWD = "";   
-   ResultSet resultset=null;
-   Connection connection=null;
-   Statement statement=null;
-      public boolean registerUser(String u, String p,String n,String e){
-       int rowAffected=0;
-       try {
-          String sql="INSERT INTO tbluser (username,name,dob,password)"
-                  + "VALUES ('"+u+"','"+n+"','"+e+"','"+p+"')";
-            dbcon query1=new dbcon();
-             Class.forName("org.apache.derby.jdbc.ClientDriver");
+    //static final String DB_PASSWD = "";   
+    ResultSet resultset = null;
+    Connection connection = null;
+    Statement statement = null;
+
+    public boolean registerUser(String u, String p, String n, String e) {
+        int rowAffected = 0;
+        try {
+            String sql = "INSERT INTO tbluser (username,name,dob,password)"
+                    + "VALUES ('" + u + "','" + n + "','" + e + "','" + p + "')";
+            dbcon query1 = new dbcon();
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
             //Get a connection
-         
-            connection = DriverManager.getConnection(DB_URL); 
-           statement=connection.createStatement();
-             rowAffected= statement.executeUpdate(sql);
-        
-      }
-       catch (Exception ex) {
-          Logger.getLogger(Users.class.getName()).log(Level.SEVERE, null, ex);
-      }
-     if ( rowAffected==1){
-         return true;
-     }else
-     {
-          return false;
-     }
-  }
+
+            connection = DriverManager.getConnection(DB_URL);
+            statement = connection.createStatement();
+            rowAffected = statement.executeUpdate(sql);
+
+        } catch (Exception ex) {
+            Logger.getLogger(Users.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        if (rowAffected == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     /**
      * @return the userid
@@ -220,6 +213,5 @@ public class Users {
     public void setTel(String tel) {
         this.tel = tel;
     }
-      
-      
+
 }
